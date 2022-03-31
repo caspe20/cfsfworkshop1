572ab0620e0f5f54b29a7077626fe877fec9c648
Mette Hansen <meha419@student.sdu.dk>
2022-03-31 13:40:16 +0200

    JSONSerialize

--- TestApplication/src/main/java/dk/sdu/mmmi/testapplication/Test.java
+++ TestApplication/src/main/java/dk/sdu/mmmi/testapplication/Test.java
@@ -7,35 +7,50 @@ package dk.sdu.mmmi.testapplication;
  */
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import dk.sdu.mmmi.st4.scfs.sensors.CO2Sensor;
 import dk.sdu.mmmi.st4.scfs.sensors.TemperatureSensor;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
  * @author Anders
  */
 public class Test {
 
-    public static void main(String[] args) {
-        CO2Sensor s = new CO2Sensor("CO2 Sensor");
-        TemperatureSensor t = new TemperatureSensor();
+    public static void main(String[] args) throws JsonProcessingException, InterruptedException {
+        //CO2Sensor s = new CO2Sensor("CO2 Sensor");
+        //TemperatureSensor t = new TemperatureSensor();
+        //
+        //s.start();
+        //t.start();
+        //
+        //for (int i = 0; i < 5; i++) {
+        //    try {
+        //        Thread.sleep(1000);
+        //    } catch (InterruptedException ex) {
+        //        Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
+        //    }
+        //    System.out.println(s.getValue());
+        //    System.out.println(t.value());
+        //}
+        //
+        //s.stop();
+        //t.stop();
+        CO2Sensor co2 = new CO2Sensor("CO2_432");
+        co2.start();
+        ObjectMapper mapper = new ObjectMapper();
+        long timeMs = System.currentTimeMillis();
+        String sensorId = co2.getId();
+        double value = co2.getValue();
+        String unit = "PPM";
+        JSONFormat JSONMessage = new JSONFormat(timeMs,sensorId,value,unit);
+        String json = mapper.writeValueAsString(JSONMessage);
+        System.out.println(json);
+        co2.stop();
         
-        s.start();
-        t.start();
-        
-        for (int i = 0; i < 5; i++) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            System.out.println(s.getValue());
-            System.out.println(t.value());
-        }
-        
-        s.stop();
-        t.stop();
+
     }
 }
