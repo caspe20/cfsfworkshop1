a87aa85a254ea7dcbeadb23209974afe37dc4015
sskovgaard <simok20@student.sdu.dk>
2022-03-31 14:09:41 +0200

    ITopic

--- service-registry-example-master/service-registry-example-master/src/main/java/Publisher.java
+++ service-registry-example-master/service-registry-example-master/src/main/java/Publisher.java
@@ -12,34 +12,11 @@ public class Publisher {
     private static final String HAZELCAST_HOST = "10.112.254.14";
     private static final int HAZELCAST_PORT = 5701;
 
-    private static final String MAP_NAME = "my-service-registry";
-    private static final String SERVICE = "CO2";
-    private static final String IP = "127.0.0.1";
 
     public static void main(String[] args) {
         HazelcastInstance hz = getHazelcastInstance();
-        MessageListener listener = new MessageListener() {
-            @Override
-            public void onMessage(Message message) {
-                System.out.println("Message recieved");
-            }
-        };
         ITopic<String> topic = hz.getTopic("CO2Sensor");
-
-        topic.addMessageListener(listener);
-
-        /*
-        Service service1 = new Service(SERVICE, IP, 8001);
-        registerService(map, service1);
-
-        Service service2 = new Service(SERVICE, IP, 8002);
-        registerService(map, service2);
-
-
-
-        queryService(map).forEach(System.out::println);
-
-         */
+        topic.publish("Test");
 
         hz.shutdown();
     }
@@ -49,25 +26,6 @@ public class Publisher {
         ClientConfig clientConfig = new ClientConfig();
         clientConfig.getNetworkConfig().addAddress(hazelcastAddress);
         return HazelcastClient.newHazelcastClient(clientConfig);
-    } 
-
-    private static void registerService(MultiMap<String,String> map, Service service){
-        String endpoint = String.format("%s:%d", service.ip, service.port);
-        map.put(service.name, endpoint);
     }
 
-    private static void registerTopic(MultiMap<String,String> map, Service service){
-        String endpoint = String.format("%s:%d", service.ip, service.port);
-        map.put(service.name, endpoint);
-    }
-
-    private static Collection<String> queryService(MultiMap<String,String> map){
-        Collection<String> servicesEndpoints = map.get(SERVICE);
-        return servicesEndpoints;
-    }
-
-    /**
-     * Service represents the type of service and its specific IP and port.
-     */
-    public record Service(String name, String ip, int port) {}
 }
